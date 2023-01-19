056d36a015e262bebfe79e6a6c0e6396354a98f2
ANSILYAS <114913715+ansilyas@users.noreply.github.com>
2023-01-19 23:40:57 +0500

    Add files via upload

---
+++ avr,let,const/js/script.js
@@ -0,0 +1,19 @@
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt("567") +BigInt("3")
+let e="hurry"
+let f=Symbol("I am a nice symble")
+let g 
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c)
+
+// Object :
+// But we can also called it Dictionaty 
+const item={
+    "Hurry":true,
+    "shubh":false,
+    "lovish":67,
+    "Ans":undefined
+}
+console.log(item["Ans","Hurry"])
